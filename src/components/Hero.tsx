b13a3862eb35d98b9360da21d984ee92092ce8df
anfrk-full <rkddmswhd1@naver.com>
2026-07-29 11:37:05 +0900

    fix : bom delete in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import { usePage } from '../context/PageContext';
 
